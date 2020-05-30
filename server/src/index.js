832213698f3904f4bbe86b39eb9dc0dceaba9647
n1ghtdev <extr1m7777@gmail.com>
2020-05-30 20:22:38 +0300

    chore: initial commit - add docker containers - add node/express/mongo backend - add nextjs react frontend

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://mongo:27017/server", { useNewUrlParser: true });
+app.use(cors({ origin: "http://localhost", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const port = 8080;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log(`SERVER running on localhost:${port}`);
+  }
+});
