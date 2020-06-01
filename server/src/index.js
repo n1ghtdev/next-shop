0eca25ae5ba30222242acaf385dc8ba40b332443
n1ghtdev <extr1m7777@gmail.com>
2020-06-01 13:58:18 +0300

    fix(server): allow cors for *

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect("mongodb://mongo:27017/server", { useNewUrlParser: true });
-app.use(cors({ origin: "http://localhost", credentials: true }));
+mongoose.connect('mongodb://mongo:27017/server', { useNewUrlParser: true });
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 const port = 8080;
 
