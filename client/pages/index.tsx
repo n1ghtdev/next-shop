de1bb2d4921fc144fb1d43c40147306fe96ca21e
n1ghtdev <extr1m7777@gmail.com>
2020-06-03 19:20:53 +0300

    chore(client): add typescript, eslint, prettier

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 export default function Home() {
   React.useEffect(() => {
     const apiCall = async () => {
-      const res = await fetch('http://localhost:8080/');
+      const res = await fetch("/api/v1/test");
       console.log(await res.text());
     };
     apiCall();
